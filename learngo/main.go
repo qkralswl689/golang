7daad27b2ff098979f270d0972b5fc1f8b90fb29
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-12-09 20:04:51 +0900

    constant and variable

--- learngo/main.go
+++ learngo/main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-fmt.Println("Hello world!")
+// func main() {
+
+// 	fmt.Println("Hello world!")
+// 	something.SayHello()
+// }
+
+func main(){
+
+	// constant
+	// const name string = "mingki"
+	// fmt.Println(name)
+
+	// variable
+	// way1
+	// var name string = "hyun"
+	// name = "ming"
+	// fmt.Println(name)
+
+	// way2
+	name := "hyun"
+ 	name = "ming"
+	fmt.Println(name)
 }
